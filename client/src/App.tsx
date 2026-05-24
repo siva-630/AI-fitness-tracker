f49accfd3cd0201998427adfbd208f4e9eeb09f6
siva-630 <siva630029@gmail.com>
2026-05-24 16:50:07 +0530

    routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,26 @@
+import { Route, Routes } from "react-router-dom"
+import Layout from "./pages/Layout"
+import Dashboard from "./pages/Dashboard"
+import FoodLog from "./pages/FoodLog"
+import ActivityLog from "./pages/ActivityLog"
+import Profile from "./pages/Profile"
 
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}  >
+          <Route index element={<Dashboard />} />
+          <Route path='food' element={<FoodLog />} />
+          <Route path='activity' element={<ActivityLog />} />
+          <Route  path='profile' element={<Profile/>}/>
+        </Route>
+
+    </Routes>
+    
+    
+    </>
   )
 }
 
--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,8 +1,9 @@
+import { Outlet } from "react-router-dom";
 
 
 const Layout = () => {
     return (
-        <div>Layout</div>
+    <Outlet/>
     )
 }
 export default Layout;
